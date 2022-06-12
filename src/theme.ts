bda7cd2815d2216e1163b45f873ca201d47a8920
hcjang <chhc130@gmail.com>
2022-06-12 20:48:59 +0900

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import {DefaultTheme} from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    textColor:'whitesmoke',
+    bgColor:'darkgray',
+    accentColor:'#9c88ff',
+    cardBgColor: "transparent",
+}
